fc529153261adfc258d1b56b590f3fb890bee56d
Nikki <dominiquelcruz@gmail.com>
2024-01-25 13:56:27 -0800

    casing of app.js updated

--- QualityRx/frontend/src/index.js
+++ QualityRx/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import App from "./App";
+import App from "./app.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
